809b07835b976ad262bdf88a8b88307ae3e27130
zhangrj <zhang_career@foxmail.com>
2026-04-16 21:24:52 +0800

    patch cleaned

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,12 +8,13 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
+use Paganini\Env\LayeredEnvLoader;
 
 $app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -21,7 +22,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (Throwable $exception, Request $request) {
-            if (!str_starts_with($request->path(), 'api/')) {
+            if (! str_starts_with($request->path(), 'api/')) {
                 return null;
             }
 
@@ -54,14 +55,12 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })->create();
 
 $app->afterLoadingEnvironment(function ($application): void {
-    if (! class_exists(\Paganini\Env\LayeredEnvLoader::class)) {
-        return;
-    }
-
-    \Paganini\Env\LayeredEnvLoader::loadEnvironmentOverlay(
+    LayeredEnvLoader::loadEnvironmentOverlay(
         $application->environmentPath(),
         $application->environmentFile()
     );
 });
+
+LayeredEnvLoader::DEFAULT_BASE_FILE;
 
 return $app;
